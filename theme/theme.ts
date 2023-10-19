ec0df351a9265f2fa96177cbbb30f6c91af0d116
sovintz <lan.sovinc@gmail.com>
2023-10-19 12:39:32 +0200

    added MUI

---
+++ theme/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#ffff00',
+        },
+        secondary: {
+            main: '#00ffff',
+        }
+    },
+    // Add other theme customization here
+});
+
+export default theme;
