82eed7017db756650294602036545f3d941b1733
sovintz <lan.sovinc@gmail.com>
2023-10-23 19:08:34 +0200

    Fixed middleware trigger

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,12 @@ import {headers} from "next/headers";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    /*const headersList = headers();
+    const headersList = headers();
     const hostname = headersList.get('host');
-    console.log(hostname)*/
-    //return NextResponse.redirect(new URL('/home', request.url))
+    console.log(hostname);
+}
+
+export const config = {
+    // The above middleware would only run for the "/" path
+    matcher: '/',
 }
